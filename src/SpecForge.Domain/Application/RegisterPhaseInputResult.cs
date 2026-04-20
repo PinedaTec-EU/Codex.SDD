2804dbba77e75dbf3d3b3a92ef23cd0880a4c2f2
jmrpineda <jmr.pineda@pinedatec.eu>
2026-04-20 21:43:07 +0200

    remove RegisterPhaseInputResult record as part of domain refactoring

--- src/SpecForge.Domain/Application/RegisterPhaseInputResult.cs
+++
@@ -1,9 +0,0 @@
-namespace SpecForge.Domain.Application;
-
-public sealed record RegisterPhaseInputResult(
-    string UsId,
-    string CurrentPhase,
-    string Status,
-    string InputArtifactPath,
-    string GeneratedArtifactPath,
-    TokenUsage? Usage);
